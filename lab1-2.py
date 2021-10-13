35088cba581f28c628e7eab46a745c7a217f483d
Dima Paryshkov <dima.paryshkov45@mail.ru>
2021-10-13 23:11:17 +0700

    lab1-2 done

--- lab1-2.py
+++ lab1-2.py
@@ -6,21 +6,29 @@ from math import sqrt
 lvl_x1=4
 lvl_x2=5
 N = 30
-alpha = 0.5
+alpha = 0.05
 
-# f = [1, x1, x2, x1**2, x2**2, x1*x2]
 #Параметры исходной модели
 tetta = [0.5, 1, 0.002, 2, 0.003, 0]
 
 #Параметры измененной модели (добавлен новый регрессор x1*x2)
 tettaChan = [0.5, 1, 0.002, 2, 0.003, 1]
 
-#Получение значения отклика
-def u(x1,x2, t = tetta):
-    return t[0] * 1 + t[1] * x1 + t[2] * x1**2 + t[3] * x2 + t[4] * x2**2 + t[5] * x1 * x2
+def u_vec(x1, x2, t = tetta):
+    x1 = np.array(x1)
+    x2 = np.array(x2)
+    f = [1, x1, x2, x1**2, x2**2, x1*x2]
+    nu = np.dot(t, f)
+    return nu
 
-#генерания факторов
-def getFactors():
+def u(x1, x2, t = tetta):
+    f = [1, x1, x2, x1**2, x2**2, x1*x2]
+    nu = 0
+    for i in range (len(f)):
+        nu = nu + t[i] * f[i]
+    return nu
+
+def Get_factors():
     X1=[]
     X2=[]
     for i in range (lvl_x1):
@@ -41,8 +49,7 @@ def getFactors():
 
     return X1,X2
 
-#Среднее значение сигнала
-def AverageSignalValue(U):
+def Get_averageSignalValue(U):
     AS_Value=0
     for i in U:
         AS_Value += i
@@ -50,17 +57,15 @@ def AverageSignalValue(U):
     AS_Value /= len(U)
     return AS_Value
 
-#Мощность сигнала
-def SignalPower(U):
+def Get_signalPower(U):
     SP = 0
-    AS_Value = AverageSignalValue(U)
+    AS_Value = Get_averageSignalValue(U)
     for i in U:
         SP += (i - AS_Value)**2
-
     SP = SP / (len(U) - 1)
     return SP
 
-def GetMatrixX():
+def Get_X():
     X=[]
 
     for i in range(N):
@@ -76,7 +81,7 @@ def GetMatrixX():
         X[i][4] = round(x2[i]**2, 5)
     return X
 
-def GetTettaR():
+def Get_tettaR():
     tettaR = []
     Xt = np.matrix(X).transpose()
     Xtemp = np.linalg.inv(Xt * np.matrix(X)) * Xt
@@ -87,6 +92,45 @@ def GetTettaR():
             temp = temp + Xtemp[i,j] * y[j]
         tettaR.append(round(temp, 5))
     return tettaR
+
+def Get_y2():
+    y2 = []
+    for i in range(0, len(X)):
+        temp = 0
+        for j in range(0, np.size(X[i]) ):
+            temp = temp + X[i][j] * tettaR[j]
+        y2.append(round(temp, 5))
+    return y2
+
+def Get_U():
+    U = []
+    for i in range(N):
+        U.append(round(u(x1[i], x2[i]),5))
+    return U
+
+def Get_sigma(U):
+    w2 = Get_signalPower(U)
+    return sqrt(0.1*(w2))
+
+def Get_e():
+    e = []
+    U=Get_U()
+    for i in range(len(U)):
+        e.append(round(np.random.normal(0, sigma),5))
+    return e
+
+def Get_y():
+    y = []
+    U=Get_U()
+    for i in range(len(U)):
+        y.append(round((U[i] + e[i]),5))
+    return y
+
+def Get_sigmaR():
+    temp = 0
+    for i in range(len(eR)):
+        temp += eR[i] * eR[i]
+    return sqrt(temp / (N - len(tettaR)))
 
 def SaveResultInFile():
     f = open("results.txt", 'w')
@@ -100,45 +144,25 @@ def SaveResultInFile():
         f.write(res)
     f.close()
 
-x1,x2=getFactors()
+x1,x2 = Get_factors()
+U = Get_U()
+sigma = Get_sigma(U)
+e = Get_e()
+y = Get_y()
 
-U=[]
+X = Get_X()
+tettaR = Get_tettaR()
 
-for i in range(N):
-    U.append(round(u(x1[i], x2[i]),5))
-
-w2 = SignalPower(U)
-sigma = sqrt(0.1*(w2))
-y=[]
-e=[]
-
-for i in range(len(U)):
-    e.append(round(np.random.normal(0, sigma),5))
-    y.append(round((U[i] + e[i]),5))
-
-X = GetMatrixX()
-
-tettaR = GetTettaR()
-
-y2 = []
-
-for i in range(0, len(X)):
-    temp = 0
-    for j in range(0, np.size(X[i]) ):
-        temp = temp + X[i][j] * tettaR[j]
-    y2.append(round(temp, 5))
-
+y2 = Get_y2()
 eR = np.array(y) - np.array(y2)
 
-temp = 0
-for i in range(len(eR)):
-    temp += eR[i] * eR[i]
-sigmaR = sqrt(temp / (N - len(tettaR)))
+sigmaR = Get_sigmaR()
 
 print(sigma)
 print(sigmaR)
 print(sigmaR/sigma)
     
-print(st.f.sf(1 - alpha, N - len(tetta), np.inf))
+ty = stats.f.sf(1 - alpha, N - len(tetta), 99999999999999)
+print(ty)
 
 SaveResultInFile()
